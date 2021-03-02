5d892752aa41446f41e26111418474cc429ee488
Student <student@PNCLB32-21.pnc.lan>
2021-03-03 00:55:49 +0700

    add win and lost

--- Labyrinth_game.py
+++ Labyrinth_game.py
@@ -1,81 +1,49 @@
 from tkinter import *
 import winsound
 root = Tk()
-
+# file of image 
 root.title("Wellcome to my game!")
 img = PhotoImage(file="C:\\Users\\student\\Desktop\\LABYRINTH_GAME\\picture\\hell.png")
 coint = PhotoImage(file="C:\\Users\\student\\Desktop\\LABYRINTH_GAME\\picture\\gold.png")
 van = PhotoImage(file="C:\\Users\\student\\Desktop\\LABYRINTH_GAME\\picture\\van.png")
 box = PhotoImage(file="C:\\Users\\student\\Desktop\\LABYRINTH_GAME\\picture\\ban.png")
-star = PhotoImage(file="C:\\Users\\student\\Desktop\\LABYRINTH_GAME\\picture\\life.png")
 enamy = PhotoImage(file="C:\\Users\\student\\Desktop\\LABYRINTH_GAME\\picture\\ee.png")
+fire = PhotoImage(file="C:\\Users\\student\\Desktop\\LABYRINTH_GAME\\picture\\red.png")
+school = PhotoImage(file="C:\\Users\\student\\Desktop\\LABYRINTH_GAME\\picture\\school.png")
+key = PhotoImage(file="C:\\Users\\student\\Desktop\\LABYRINTH_GAME\\picture\\key1.png")
 w = 1320
-h = 750
-my_canvas = Canvas(root, width=w, height=h, bg ="black")
-my_canvas.pack()
+h = 685
+my_canvas = Canvas(root, width=w, height=h)
+my_canvas.pack(side=BOTTOM)
 
-# # student move
-x = 45
-y = 42
-my_cicle = my_canvas.create_image(x,y, image=img)
-
-# star 
-size = 20
-xstar = 30
-ystar = 720
-my_canvas.create_image(30,720, image=star)
-
-
-
-# function
-
-def right(event):
-     global x,y 
-    
-     x = 30
-     y = 0
-     my_canvas.move(my_cicle, x,y)
-def down(event):
-     global x,y 
-    
-     x = 0
-     y = 30
-     my_canvas.move(my_cicle, x,y)
-def up(event):
-     global x,y 
-     x = 0
-     y = -30
-     my_canvas.move(my_cicle, x,y)
-     print(x,y)
-
-
+# array2D of graphic
 size= 30 
 array2D =[
      [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-     [1,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-     [1,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-     [1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,4,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
-     [1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1,1,2,0,0,0,0,0,0,0,0,0,0,1],
-     [1,0,1,0,0,0,0,0,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1],
-     [1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,2,0,0,0,0,0,0,0,0,1],
+     [1,10,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+     [1,0,1,0,1,1,1,1,1,1,1,1,1,0,1,0,0,1,0,0,2,0,0,0,0,0,0,1,0,0,6,0,0,0,0,0,0,0,0,0,0,0,0,1],
+     [1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,2,0,4,0,7,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+     [1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,2,0,0,0,0,0,0,1,0,0,1,1,2,0,0,0,0,0,0,0,0,0,0,1],
+     [1,0,1,0,1,0,0,0,1,1,1,1,1,1,1,0,0,1,0,0,6,0,0,0,0,0,0,1,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1],
+     [1,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,1,0,0,1,0,0,1,2,0,0,0,0,0,0,0,0,1],
      [1,0,0,2,0,2,0,2,0,0,0,0,0,0,1,0,0,1,0,0,1,1,1,1,1,1,1,1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,1],
-     [1,0,0,1,1,1,1,1,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,2,0,0,0,0,0,0,1],
+     [1,0,0,1,1,1,1,1,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,8,0,0,1,2,0,0,0,0,0,0,1],
      [1,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1],
      [1,1,1,1,0,0,1,0,0,0,0,1,0,0,1,0,0,1,0,0,1,1,0,0,1,1,0,0,0,0,1,0,0,0,0,0,0,1,2,0,0,0,0,1],
-     [1,2,0,1,0,0,0,0,0,1,1,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1],
+     [1,0,0,1,0,0,0,0,0,1,1,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1],
      [1,0,0,1,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,2,0,0,1],
-     [1,2,0,1,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1],
+     [1,0,0,1,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1],
      [1,0,0,1,1,1,1,1,1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,1],
-     [1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1],
-     [1,0,0,0,0,0,0,0,5,0,5,0,5,0,5,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
-     [1,2,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1],
+     [1,0,0,0,0,0,0,0,0,0,0,5,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,1],
+     [1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,1,0,0,0,5,0,0,5,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1],
      [1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-     [1,2,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-     [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+     [1,0,5,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+     [1,0,0,0,0,0,6,0,0,0,0,0,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
      [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
      [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 ]
-
+# Display graphic of game
 def graphic() :
      global gold
      for num in range(len(array2D)):
@@ -89,28 +57,169 @@ def graphic() :
                elif array2D[num][i] == 4:
                     my_canvas.create_image(15+(30*i),15+(30*num), image=van)
              
-                    
-def enamymove():
-     for num in range(len(array2D)):
-          for i in range(len(array2D[num])):
-               if array2D[num][i] == 5:
-                    oval=my_canvas.create_image(15+(30*i),8+(30*num), image=enamy)
-#                canvas.moveto(oval,2,0)
-#     canvas.after(50, lambda: enamymove())
-def left(event):
-     global x,y 
-     x = -30
-     y = 0
-                   
-     my_canvas.move(my_cicle, x,y)
-enamymove()
-graphic()
-#key
-root.bind("<a>",left)
-root.bind("<d>",right)
-root.bind("<w>",up)
-root.bind("<s>",down)
+               elif array2D[num][i] == 6:
+                    my_canvas.create_image(15+(30*i),25+(30*num), image=fire)
+               elif array2D[num][i] == 10:
+                    my_canvas.create_image(15+(30*i),15+(30*num), image=img)
+               elif array2D[num][i] == 5:
+                    my_canvas.create_image(15+(30*i),10+(30*num), image=enamy)
+     
+               elif array2D[num][i] == 7:
+                    my_canvas.create_image(15+(30*i),10+(30*num), image=school)
+               elif array2D[num][i] == 8:
+                    my_canvas.create_image(15+(30*i),10+(30*num), image=key)
 
-winsound.PlaySound("sound\\vanda.wav", winsound.SND_LOOP | winsound.SND_ASYNC)
+               
+    
+
+
+# Move position of player
+
+def PosirionPacman(array2D):
+     for i in range(len(array2D)):
+          for n in range(len(array2D[i])):
+               if array2D[i][n] ==10:
+                    postion=[i,n]
+     return postion
+def enamyDie():
+     position=PosirionPacman(array2D)
+     row=position[0]
+     column=position[1]
+     if array2D[row][column] == 5 :
+          array2D[row][column]=0
+          array2D[row][column-1]= 5
+          my_canvas.after(400, lambda:graphic())
+          print('Good')
+
+enamyDie()
+def moveRight(event):
+     position=PosirionPacman(array2D)
+     row=position[0]
+     column=position[1]
+     if array2D[row][column+1] != 5 and array2D[row][column+1] != 6:
+          if array2D[row][column+1] == 2:
+               array2D[row][column]= 0
+               array2D[row][column+1]=10
+               coin()
+          elif array2D[row][column+1] == 7:
+               my_canvas.delete("all")
+               youwin()
+               array2D[row][column]=0
+               array2D[row][column+1]=10
+          elif array2D[row][column+1]!= 1:
+               array2D[row][column]= 0
+               array2D[row][column+1]=10
+          my_canvas.delete("all")
+     if array2D[row][column+1] == 5 :
+          array2D[row][column]= 0
+          array2D[row][column+1]=10
+          youLost()
+  
+     graphic()
+
+def moveLeft(event):
+     position=PosirionPacman(array2D)
+     row=position[0]
+     column=position[1]
+     if array2D[row][column-1] != 5 and array2D[row][column-1] != 6:
+          if array2D[row][column-1] == 2:
+               array2D[row][column]= 0
+               array2D[row][column-1]=10
+               coin()
+          elif array2D[row][column-1] == 7:
+               my_canvas.delete("all")
+               youwin()
+               array2D[row][column]=0
+               array2D[row][column]=10
+          elif array2D[row][column-1]!= 1:
+               array2D[row][column]=0
+               array2D[row][column-1]=10
+          my_canvas.delete("all")
+     if array2D[row][column-1] == 5 :
+          my_canvas.delete("all")
+          array2D[row][column]= 0
+          array2D[row][column-1]=10
+          youLost()
+     graphic()
+def moveUp(event):
+     position=PosirionPacman(array2D)
+     row=position[0]
+     column=position[1]
+     if array2D[row-1][column] != 5 and array2D[row-1][column] != 6:
+          if array2D[row-1][column] == 2:
+               array2D[row][column]= 0
+               array2D[row-1][column]=10
+               coin()
+          elif array2D[row-1][column] == 7:
+               my_canvas.delete("all")
+               youwin()
+               array2D[row][column]=0
+               array2D[row-1][column]=10
+          elif array2D[row-1][column]!= 1:
+               array2D[row][column]=0
+               array2D[row-1][column]=10
+          my_canvas.delete("all")
+     if array2D[row-1][column] == 5 :
+          my_canvas.delete("all")
+          array2D[row][column]= 0
+          array2D[row-1][column]=10
+          youLost()
+     graphic()
+def moveDown( event):
+     position=PosirionPacman(array2D)
+     row=position[0]
+     column=position[1]
+     if array2D[row+1][column] != 5 and array2D[row+1][column] != 6:
+          if array2D[row+1][column] == 2:
+               array2D[row][column]= 0
+               array2D[row+1][column]=10
+               coin()
+          elif array2D[row+1][column] == 7:
+               my_canvas.delete("all")
+               youwin()
+               array2D[row][column]=0
+               array2D[row+1][column]=10
+          elif array2D[row+1][column]!= 1:
+               array2D[row][column]=0
+               array2D[row+1][column]=10
+          my_canvas.delete("all")
+     if array2D[row+1][column] == 5 :
+          my_canvas.delete("all")
+          array2D[row][column]= 0
+          array2D[row+1][column]=10
+          youLost()
+     graphic()
+
+
+
+# count coin of player 
+count = 0 
+def coin() :
+     global count,Label
+     winsound.PlaySound("sound\\whenkak.wav", winsound.SND_ASYNC | winsound.SND_ASYNC)
+     count += 1
+    
+# Result of game win or lost 
+def youwin() :
+     global my_canvas,w,h
+     my_canvas.create_text(w/2, 20+(h/2) ,font=("Purisa",25),text= "YOU WON !!!")
+     winsound.PlaySound("sound\\coin1.wav", winsound.SND_ASYNC | winsound.SND_ASYNC)
+     my_canvas = Canvas(root, width=w, height=h) 
+     label = Label(root,text=("coin:",count))
+     labe =label.config(font=("Monospace",20))
+     label.place(x=620, y=400)
+def youLost() :
+     global my_canvas,w,h
+     winsound.PlaySound("sound\\gameover.wav", winsound.SND_ASYNC | winsound.SND_ASYNC)
+     my_canvas.create_text(w/2, 20+(h/2) ,font=("Purisa",25),text= "YOU LOST!")
+     my_canvas = Canvas(root, width=w, height=h) 
+# move position of player 
+root.bind("<a>",moveLeft)
+root.bind("<d>",moveRight)
+root.bind("<w>",moveUp)
+root.bind("<s>",moveDown)
+graphic()
+# sound game 
+# winsound.PlaySound("sound\\vanda.wav", winsound.SND_LOOP | winsound.SND_ASYNC)
 # mainloop
 root.mainloop()
